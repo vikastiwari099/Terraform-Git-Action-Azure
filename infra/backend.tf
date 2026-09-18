fe7c20bc694f9dba0345240c5b83b01b36170e89
Vikas Tiwari <85790213+vikastiwari099@users.noreply.github.com>
2026-09-18 15:56:26 +0530

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -7,7 +7,7 @@ terraform {
     use_oidc             = true
     use_azuread_auth     = true # Entra ID auth to the blob data plane (recommended over access keys)
     resource_group_name  = "rg-tfstate"
-    storage_account_name = "tfstateXXXXXX" # <- replace with output from bootstrap
+    storage_account_name = "tfstatevinfo123" # <- replace with output from bootstrap
     container_name       = "tfstate"
     key                  = "infra/terraform.tfstate"
   }
